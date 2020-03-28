d8af10483d743d3f43af7b259a0053a669ed4971
HopefulTentacles <1343286547@qq.com>
2020-03-28 22:11:24 +0800

    who's your father? tell me !

--- test_github/src/main/java/com/itheima/pojo/XuChaoChuang.java
+++ test_github/src/main/java/com/itheima/pojo/XuChaoChuang.java
@@ -4,6 +4,6 @@ import java.sql.SQLOutput;
 
 public class XuChaoChuang {
     public static void main(String[] args) {
-        System.out.println("who's yout dady?");
+        System.out.println("who's your father?");
     }
 }
